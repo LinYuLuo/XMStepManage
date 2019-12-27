e8c32aaff79d2507f7bbf7059b23d69251aadd3a
XyLin <XyLin>
2019-12-28 00:49:41 +0800

    完成步数管理&&经测试查明Android10(MIUI11)无法进行修改(权限不足)--1912280049

---
+++ app/src/main/java/cn/xylin/miui/step/manage/MainActivity.java
@@ -0,0 +1,109 @@
+package cn.xylin.miui.step.manage;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author XyLin
+ * @date 2019/12/27
+ **/
+public class MainActivity extends Activity {
+    private final String ID = "_id";
+    private final String BEGIN_TIME = "_begin_time";
+    private final String END_TIME = "_end_time";
+    private final String MODE = "_mode";
+    private final String STEPS = "_steps";
+    private final String[] QUERY_FILED = {ID, BEGIN_TIME, END_TIME, MODE, STEPS};
+    private final String[] PERMISSIONS = {"miui.permission.WRITE_STEPS", "miui.permission.READ_STEPS"};
+    private final String DEFAULT_ORDER = "_id ASC";
+    private final Uri STEP_URI = Uri.parse("content://com.miui.providers.steps/item");
+    private TextView tvTodaySteps;
+    private EditText edtAddSteps;
+    private int todayStepCount;
+    private long clickTime = 0L;
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvTodaySteps = findViewById(R.id.tvTodaySteps);
+        edtAddSteps = findViewById(R.id.edtAddSteps);
+        getTodayStep();
+    }
+
+    private void getTodayStep() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Cursor cursor = getContentResolver().query(STEP_URI, QUERY_FILED, null, null, DEFAULT_ORDER);
+                    long todayBeginTime = timeFormat.parse(getTodayTime(true)).getTime();
+                    long todayEndTime = timeFormat.parse(getTodayTime(false)).getTime();
+                    if (cursor != null && cursor.moveToNext()) {
+                        todayStepCount = 0;
+                        while (cursor.moveToNext()) {
+                            if (cursor.getLong(1) > todayBeginTime && cursor.getLong(2) < todayEndTime && cursor.getInt(3) == 2) {
+                                todayStepCount += cursor.getInt(4);
+                            }
+                        }
+                        cursor.close();
+                    }
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvTodaySteps.setText(String.format(getString(R.string.text_today_steps), todayStepCount));
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private String getTodayTime(boolean flag) {
+        return String.format("%s%s", timeFormat.format(System.currentTimeMillis()).substring(0, 11), flag ? "00:00:00" : "23:59:59");
+    }
+
+    public void startStepAdd(View view) {
+        try {
+            int addSteps = Integer.parseInt(edtAddSteps.getText().toString());
+            ContentValues values = new ContentValues();
+            values.put(BEGIN_TIME, (System.currentTimeMillis() - 1200000L));
+            values.put(END_TIME, System.currentTimeMillis());
+            values.put(MODE, 2);
+            values.put(STEPS, addSteps);
+            getContentResolver().insert(STEP_URI, values);
+            Toast.makeText(this, R.string.toast_add_steps_success, Toast.LENGTH_SHORT).show();
+            getTodayStep();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, getString(R.string.toast_add_steps_failed) + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - clickTime) > 2000L) {
+                clickTime = System.currentTimeMillis();
+                Toast.makeText(this, R.string.toast_exit_app, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
