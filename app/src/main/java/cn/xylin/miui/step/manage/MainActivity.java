ede1649e4bf85d467285f97fe580d8af0dd46c64
XyLin <XyLin>
2019-12-28 12:55:19 +0800

    修复读取今日步数数据可能错误的原因&&无法修改步数的原因可能为未对设备进行核心破解--1912281255

--- app/src/main/java/cn/xylin/miui/step/manage/MainActivity.java
+++ app/src/main/java/cn/xylin/miui/step/manage/MainActivity.java
@@ -1,10 +1,13 @@
 package cn.xylin.miui.step.manage;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -24,8 +27,6 @@ public class MainActivity extends Activity {
     private final String MODE = "_mode";
     private final String STEPS = "_steps";
     private final String[] QUERY_FILED = {ID, BEGIN_TIME, END_TIME, MODE, STEPS};
-    private final String[] PERMISSIONS = {"miui.permission.WRITE_STEPS", "miui.permission.READ_STEPS"};
-    private final String DEFAULT_ORDER = "_id ASC";
     private final Uri STEP_URI = Uri.parse("content://com.miui.providers.steps/item");
     private TextView tvTodaySteps;
     private EditText edtAddSteps;
@@ -48,10 +49,10 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 try {
-                    Cursor cursor = getContentResolver().query(STEP_URI, QUERY_FILED, null, null, DEFAULT_ORDER);
+                    Cursor cursor = getContentResolver().query(STEP_URI, QUERY_FILED, null, null, null);
                     long todayBeginTime = timeFormat.parse(getTodayTime(true)).getTime();
                     long todayEndTime = timeFormat.parse(getTodayTime(false)).getTime();
-                    if (cursor != null && cursor.moveToNext()) {
+                    if (cursor != null) {
                         todayStepCount = 0;
                         while (cursor.moveToNext()) {
                             if (cursor.getLong(1) > todayBeginTime && cursor.getLong(2) < todayEndTime && cursor.getInt(3) == 2) {
@@ -70,27 +71,34 @@ public class MainActivity extends Activity {
                     e.printStackTrace();
                 }
             }
-        }).start();
+        }).
+
+                start();
     }
 
     private String getTodayTime(boolean flag) {
         return String.format("%s%s", timeFormat.format(System.currentTimeMillis()).substring(0, 11), flag ? "00:00:00" : "23:59:59");
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     public void startStepAdd(View view) {
         try {
-            int addSteps = Integer.parseInt(edtAddSteps.getText().toString());
-            ContentValues values = new ContentValues();
-            values.put(BEGIN_TIME, (System.currentTimeMillis() - 1200000L));
-            values.put(END_TIME, System.currentTimeMillis());
-            values.put(MODE, 2);
-            values.put(STEPS, addSteps);
-            getContentResolver().insert(STEP_URI, values);
-            Toast.makeText(this, R.string.toast_add_steps_success, Toast.LENGTH_SHORT).show();
-            getTodayStep();
+                int addSteps = Integer.parseInt(edtAddSteps.getText().toString());
+                ContentValues values = new ContentValues();
+                values.put(BEGIN_TIME, (System.currentTimeMillis() - 1200000L));
+                values.put(END_TIME, System.currentTimeMillis());
+                values.put(MODE, 2);
+                values.put(STEPS, addSteps);
+                getContentResolver().insert(STEP_URI, values);
+                Toast.makeText(this, R.string.toast_add_steps_success, Toast.LENGTH_SHORT).show();
+                getTodayStep();
         } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, getString(R.string.toast_add_steps_failed) + e.toString(), Toast.LENGTH_SHORT).show();
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.toast_add_steps_failed)
+                    .setMessage(e.toString())
+                    .setPositiveButton(R.string.btn_ok, null)
+                    .create()
+                    .show();
         }
     }
 
